b37cd357edf7542fd8d898b2c83c64bc58b5d0a8
JUNGmoon123 <jungpro567@gmail.com>
2024-05-14 09:28:08 +0900

    JWT test

---
+++ src/test/java/com/koreait/jwt_2024_05/JwtTests.java
@@ -0,0 +1,18 @@
+package com.koreait.jwt_2024_05;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class JwtTests {
+    @Value("${custom.jwt.secretKey")
+    private String secretKey;
+
+    @Test
+    @DisplayName("secretKey가 존재해야 함.")
+    void t1(){
+        assertThat(secretKey).isNotNull();
+    }
+}
